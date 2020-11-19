7168e2ac79ac2dee20dd2c40bf34991aac5f7eae
Conor Mack <cmckeown101@gmail.com>
2020-11-19 11:06:59 +0000

    Finished section 02 Datatypes

--- Work/02_Datatypes/portfolio_cost.py
+++ Work/02_Datatypes/portfolio_cost.py
@@ -1,5 +1,6 @@
 
 import csv
+from pprint import pprint
 
 def portfolio_cost(filename):
     row_list = []
@@ -8,9 +9,14 @@ def portfolio_cost(filename):
     with open(fn, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
-        for row in rows:
-            t = {'name': row[0], 'shares': int(row[1]), 'price':float(row[2])}
-            row_list.append(t)
+        for rowno, row in enumerate(rows):
+            record = dict(zip(headers, row))
+            try:
+                t = {'name': record["name"], 'shares': record["shares"], 'price': record["price"]}
+                row_list.append(t)
+            except ValueError:
+                print(f'Row {rowno}: Couldn\'t convert: {row}')
         return row_list
 
-portfolio_cost('portfolio.csv')
+res = portfolio_cost('portfolio.csv')
+pprint(res)
--- Work/report.py
+++ Work/report.py
@@ -3,15 +3,23 @@
 # Exercise 2.4
 import csv
 
-def portfolio_cost(filename):
+
+def read_portfolio(filename):
     row_list = []
-    
+
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
-        for row in rows:
-            t = (row[0], int(row[1]), float(row[2]))
-            row_list.append(t)
+        types = [str, int, float]
+        for rowno, row in enumerate(rows):
+            row_with_types = list(zip(types, row))
+            converted_row = [func(val) for func, val in row_with_types]
+            record = dict(zip(headers, converted_row))
+            try:
+                t = {'name': record["name"], 'shares': record["shares"], 'price': record["price"]}
+                row_list.append(t)
+            except ValueError:
+                print(f'Row {rowno}: Couldn\'t convert: {row}')
         return row_list
 
-# portfolio_cost('Work/Data/portfolio.csv')
+read_portfolio('Data/portfolio.csv')
