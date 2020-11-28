af17236b8c4ff42773d31d8027c4afc04f4c24b2
Conor Mack <conoramck@gmail.com>
2020-11-28 20:33:28 +0000

    Tidy up Folder Structure

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,24 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import csv
+from pprint import pprint
+
+def portfolio_cost(filename):
+    row_list = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for rowno, row in enumerate(rows):
+            record = dict(zip(headers, row))
+            try:
+                t = {'name': record["name"], 'shares': record["shares"], 'price': record["price"]}
+                row_list.append(t)
+            except ValueError:
+                print(f'Row {rowno}: Couldn\'t convert: {row}')
+        return row_list
+
+res = portfolio_cost('portfolio.csv')
+pprint(res)
--- Work/report.py
+++ Work/report.py
@@ -4,10 +4,15 @@
 import csv
 
 
+def dataDir(filename):
+    import pathlib
+    return f'{pathlib.Path(__file__).parent.absolute()}\Data\{filename}'
+
+
 def read_portfolio(filename):
     row_list = []
 
-    with open(filename, 'rt') as f:
+    with open(dataDir(filename), 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
         types = [str, int, float]
@@ -22,4 +27,40 @@ def read_portfolio(filename):
                 print(f'Row {rowno}: Couldn\'t convert: {row}')
         return row_list
 
-read_portfolio('Data/portfolio.csv')
+
+def read_prices(filename):
+    d = {}
+
+    with open(dataDir(filename), 'rt') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if len(row) > 0:
+                name, price = row
+                d[name] = price
+        return d
+
+def make_report(portfolio, prices):
+    stock_valuation = []
+    for stock in portfolio:
+        if stock['name'] in prices:
+            old_price = float(stock['price'])
+            new_price = float(prices[stock['name']])
+            change = new_price - old_price
+            new_stock = (stock['name'], stock['shares'], float(prices[stock['name']]), change)
+            stock_valuation.append(new_stock)
+    return stock_valuation
+
+def print_report(report, headers):
+    print('%10s %10s %10s %10s' % headers)
+    print(('{dash} '.format(dash='-'*10)) * 4)
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} ${price:>10.2f} {change:>10.2f}')
+
+def portfolio_report(portfolioFile, priceFile):
+    portfolio = read_portfolio(portfolioFile)
+    prices = read_prices(priceFile)
+
+    report = make_report(portfolio, prices)
+    print_report(report, ('Name', 'Shares', 'Price', 'Change'))
+
+portfolio_report('portfolio.csv', 'prices.csv')
